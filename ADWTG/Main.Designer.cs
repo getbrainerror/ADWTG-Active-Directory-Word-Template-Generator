055045c8322843f83841b523392e94d84b0791bc
getbrainerror <oleurbigkeit@gmail.com>
2019-08-05 19:16:10 +0200

    First commit
    First commit

---
+++ ADWTG/Main.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace ADWTG
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 61);
+            this.progressBar1.MarqueeAnimationSpeed = 40;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(366, 22);
+            this.progressBar1.Step = 25;
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBar1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(366, 46);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Please wait until ADWTG is done with the creation of the template...";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // backgroundWorker
+            // 
+            this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BackgroundWorker_DoWork);
+            this.backgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BackgroundWorker_RunWorkerCompleted);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 100);
+            this.ControlBox = false;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ADWTG - Generate Template...";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.Shown += new System.EventHandler(this.Main_Shown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+    }
+}
+
---
+++ ADWTG/Main.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.DirectoryServices;
+using System.DirectoryServices.AccountManagement;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop;
+
+namespace ADWTG
+{
+    public partial class Main : Form
+    {
+        private string TemplatePath;
+        private string SavePath;
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            if (Properties.Settings.Default.DomainServer == String.Empty)
+            {
+                MessageBox.Show("Please Conigure this Application before using it.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Close();
+            }
+        }
+
+
+        private void GenerateTemplate(string savePath, string templatePath)
+        {
+            //Yeah not pretty but ¯\_(ツ)_/¯
+            try
+            {
+                //Support for Enviroment Vars like %username%
+                object saveAsObj = Environment.ExpandEnvironmentVariables(savePath);
+                templatePath = Environment.ExpandEnvironmentVariables(templatePath);
+
+                DirectoryEntry de = new DirectoryEntry(Properties.Settings.Default.DomainServer);
+                // Authentication details
+                de.AuthenticationType = AuthenticationTypes.FastBind;
+                DirectorySearcher DirectorySearcher = new DirectorySearcher(de);
+                DirectorySearcher.ClientTimeout = TimeSpan.FromSeconds(30);
+                // load all properties
+                DirectorySearcher.PropertiesToLoad.Add("*");
+                //Use Current logged in SamAccountName as filter            
+                DirectorySearcher.Filter = "(sAMAccountName=" + UserPrincipal.Current.SamAccountName + ")";
+                SearchResult result = DirectorySearcher.FindOne(); // There should only be one entry
+                if (result != null)
+                {
+                    //Word init
+                    //Source: https://www.techrepublic.com/blog/how-do-i/how-do-i-modify-word-documents-using-c/
+                    object missing = System.Reflection.Missing.Value;
+                    Microsoft.Office.Interop.Word.Application wordApp = new Microsoft.Office.Interop.Word.Application();
+                    Microsoft.Office.Interop.Word.Document document = null;
+                    if (File.Exists(templatePath))
+                    {
+                        object file = (object)templatePath;
+                        object readOnly = true;
+                        object isVisisble = false;
+                        document = wordApp.Documents.Open(ref file, ref missing,
+                            ref readOnly, ref missing, ref missing, ref missing,
+                            ref missing, ref missing, ref missing, ref missing,
+                            ref missing, ref isVisisble, ref missing, ref missing,
+                            ref missing, ref missing
+                            );
+
+                        document.Activate();
+
+
+
+                        //Loop through each properties
+
+                        foreach (string propname in result.Properties.PropertyNames)
+                        {
+                            foreach (Object objValue in result.Properties[propname])
+                            {
+                                this.FindAndReplace(wordApp, "<" + propname.ToLower() + ">", objValue);
+                            }
+                        }
+                        document.SaveAs2(ref saveAsObj, Microsoft.Office.Interop.Word.WdSaveFormat.wdFormatXMLTemplate);
+                        document.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Couldn't find Template File " + templatePath, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+
+
+                }
+                else
+                {
+                    //Didnt find anything, which should be impossible but safe is safe
+                    MessageBox.Show("Couldn't find any entries for " + UserPrincipal.Current.SamAccountName, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Unknown Error: " + e.Message , "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);                
+            }
+                    
+        }
+
+        private void FindAndReplace(Microsoft.Office.Interop.Word.Application WordApp, object findText, object replaceVithText)
+        {
+            object matchCase = true;
+            object matchWholeWord = true;
+            object matchVildCards = false;
+            object matchSoundsLike = false;
+            object matchAllWordForms = false;
+            object forward = true;
+            object format = false;
+            object matchKashida = false;
+            object matchDiacritics = false;
+            object matchAlefHamza = false;
+            object matchControl = false;
+            object replace = 2;
+            object wrap = 1;
+
+            WordApp.Selection.Find.Execute(ref findText, ref matchCase, ref matchWholeWord, ref matchVildCards, ref matchSoundsLike,
+                ref matchAllWordForms, ref forward, ref wrap, ref format, ref replaceVithText, ref replace, ref matchKashida,
+                ref matchDiacritics, ref matchAlefHamza, ref matchControl);
+            }
+
+        private void Main_Shown(object sender, EventArgs e)
+        {
+            string[] args = Environment.GetCommandLineArgs();
+            if (args.Length == 3)
+            {
+                //1 arg == filename
+                //2 arg == templatePath
+                //3 args == savePath
+                TemplatePath = args[1];
+                SavePath = args[2];
+                backgroundWorker.RunWorkerAsync();
+            } else {				
+				 MessageBox.Show("Usage: adwtg.exe templatepath savepath" + e.Message , "Info", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				 this.Close();
+				 
+			}
+        }
+
+        private void BackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            GenerateTemplate(SavePath, TemplatePath);
+        }
+
+        private void BackgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
